2d231f0110db1a01d7758c13c363f991976f923c
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-01-13 16:52:50 +0300

    Edit tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,10 @@ import unittest
 my_model = BaseModel()
 
 class TestBaseModel(unittest.TestCase):
+    def test_base_class(self):
+        self.assertEqual(type(my_model), BaseModel)
+
+class TestSave(unittest.TestCase):
     def test_update(self):
         my_model.name = "My First Model"
         my_model.my_number = 89
@@ -15,6 +19,7 @@ class TestBaseModel(unittest.TestCase):
         second = my_model.updated_at
         self.assertFalse(first == second)
 
+class TestDictToJson(unittest.TestCase):
     def test_dict_to_json(self):
         my_model_json = my_model.to_dict()
         print(my_model_json)
@@ -22,3 +27,4 @@ class TestBaseModel(unittest.TestCase):
         for key in my_model_json.keys():
             print("\t{}: ({}) - {}".format(
                 key, type(my_model_json[key]), my_model_json[key]))
+        self.assertTrue(my_model_json.__class__)
