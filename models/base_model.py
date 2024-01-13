6691ff15cfd303a31ec924defb19f8ca786203ba
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-01-13 16:02:14 +0300

    Make the base_model class

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Make base module for other classes """
+
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """This is the parent class/model"""
+
+    def __init__(self):
+        """initialize the BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Display the string representation of the BaseModel class"""
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dc = self.__dict__
+        dc['__class__'] = 'BaseModel'
+        dc['created_at'] = dc['created_at'].isoformat()
+        dc['updated_at'] = dc['updated_at'].isoformat()
+        return dc
