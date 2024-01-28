a5808978e0f7a744cfaaba8173700823db5358d3
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-01-29 00:23:49 +0300

    Make a basic cosole

--- console.py
+++ console.py
@@ -10,26 +10,15 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, line):
         """ quit the proto tool"""
-        print('see you!')
         return True
     def help_quit(self):
         """describe what quit does"""
-        print('Ibra')
+        print('some info')
 
     def do_EOF(self, line):
         """exit gracefully """
         print()
         return True
-    def message(self, mess):
-        print(mess)
 
-    def do_greet(self, perso):
-        """ greet perso"""
-        if perso:
-            print('Hello,')
-        else:
-            print('Hi')
-
-        print(f'I Love you so much {perso}')
 if __name__ == '__main__':
     HBNBCommand().cmdloop(intro='Welcome!')
