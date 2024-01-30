d0d32c8e83548d6344769cf52b22b2651ea1341f
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-01-30 16:18:27 +0300

    Update my console

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """ my console """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 import readline
 
 
@@ -12,6 +14,7 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """ quit the proto tool"""
         return True
+
     def help_quit(self):
         """describe what quit does"""
         print('some info')
@@ -24,6 +27,102 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ do nothing """
         pass
+
+    def do_create(self, cmmd):
+        """ create a base model object """
+
+        if cmmd and cmmd != BaseModel().__class__.__name__:
+            print("** class doesn't exist **")
+        elif cmmd:
+            b = BaseModel()
+            b.save()
+            print(b.id)
+        else:
+            print('** class name missing **')
+
+    def do_show(self, cmmd):
+        if cmmd:
+            arr = cmmd.split()
+
+            if arr[0] != 'BaseModel':
+                print("** class does't exist **")
+            elif len(arr) != 2:
+                print("** instace id missing **")
+            else:
+                st = arr[0] + '.' + arr[1]
+                oj = storage.all()
+                if st in oj:
+                    print(oj[st])
+                else:
+                    print("** no instance found **")
+        else:
+            print("** class name missing **")
+
+    def do_destroy(self, cmmd):
+        """ delete an object """
+        if cmmd:
+            arr = cmmd.split()
+
+            if arr[0] != 'BaseModel':
+                print("** class doesn't exist **")
+            elif len(arr) == 2:
+                st = arr[0] + '.' + arr[1]
+
+                oj = storage.all()
+
+                if st in oj:
+                    oj.pop(st)
+                    storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class name missing **")
+
+    def do_all(self, cmmd):
+        """ print all present object instances """
+        oj = storage.all()
+
+        if cmmd and cmmd != 'BaseModel':
+            print("** class doesn't exist **")
+        elif cmmd:
+            for x in oj:
+                if "BaseModel" in x:
+                    print(oj[x])
+        else:
+            for x in oj:
+                print(oj[x])
+
+    def do_update(self, cmmd):
+        """ update the objects and saves them to file """
+        oj = storage.all()
+
+        if cmmd:
+            arr = cmmd.split()
+
+            if arr[0] == "BaseModel":
+                if len(arr) >= 2:
+                    st = arr[0] + '.' + arr[1]
+
+                    if st in oj:
+                        object_to_update = oj[st]
+                        if len(arr) >= 4:
+                            object_to_update.update({arr[2]: arr[3]})
+                            storage.save()
+                        elif len(arr) == 3:
+                            print("** value missing **")
+                        else:
+                            print("** attribute name missing **")
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
